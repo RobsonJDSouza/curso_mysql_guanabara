b772d9d17a02782116fed6501859fa58571d8064
Robson Souza <robson_souzaj@hotmail.com>
2022-04-17 08:56:57 -0300

    Atualização

--- 02_comando.sql
+++ 02_comando.sql
@@ -10,243 +10,184 @@
 	SQL Editor
 	safe Updates(rejects Updates and Deletes with no restrictions)
 
-create database consulta -- cria um novo banco de dados
-	default character set utf8 -- padrão de acentuação 
-    default collate utf8_general_ci;
+CREATE database consulta -- cria um novo banco de dados
+	DEFAULT character SET utf8 -- padrão de acentua ão 
+    DEFAULT collate utf8_general_ci;
 
-use consulta; -- acessar o banco de dados consulta
+USE consulta; -- acessar o banco de dados consulta
 
-create table pessoas( -- criar tabela para banco de dados
+CREATE TABLE pessoas( -- criar tabela para banco de dados
 	id int not null auto_increment, -- não precisa dar a informação. o BD irá colocar automáticamente
-	nome varchar (30),
+	nome VARCHAR (30),
 	nascimento date,
 	sexo enum ('F', 'M'),
 	peso decimal (5, 2),
 	altura decimal (3,2),
-	nascionalidade varchar (30) default 'Brasil', -- default - caso não colocar a informação o BD irá colocar o padrão
-	primary key (id) -- coloca a chave primária para a tabela
-) default charset = utf8;
+	nascionalidade VARCHAR (30) DEFAULT 'Brasil', -- DEFAULT - caso não colocar a informação o BD irá colocar o padrão
+	PRIMARY KEY (id) -- coloca a chave primária para a tabela
+) DEFAULT charset = utf8;
 
-create table if not exists cursos(   -- if not exists - caso tiver uma tabela cursos o BD não deixará sobrescrever
-	nome varchar(30) not null unique,  -- not null obriga escrever no campo - Constranger unique/ terá um nome apenas no registro
+CREATE TABLE if not EXISTS cursos(   -- if not EXISTS - caso tiver uma tabela cursos o BD não deixará sobrescrever
+	nome VARCHAR(30) not null unique,  -- not null obriga escrever no campo - Constranger unique/ terá um nome apenas no registro
 	descricao text,
 	carga int unsigned,  -- unsigned - não poderá ter sinais no registro
 	totalaula int unsigned,
-	ano year default '2022'
-) default charset = utf8;  -- charset - para não termos problema de acentuaçãoptimize
+	ano year DEFAULT '2022'
+) DEFAULT charset = utf8;  -- charset - para não termos problema de acentuaçãoptimize
 
 show databases; -- mostrar todos bancos existentes
 show tables; -- mostrar todas tabelas exitentes no schema
 describe tabela; -- mostras as infomações da tabela
-drop database databases; -- excluir um banco/schema
+DROP database databases; -- excluir um banco/schema
 
 -- inserir dados do usuário
-insert into nome_da_tabela
+INSERT INTO nome_da_tabela
 	(id, nome, nascimento, sexo, peso, altura, nascionalidade) -- todos os campos da tabela
-values
-	(default, 'Robson', '1984-08-20', 'M', '84.0', '1.79', 'Brasil');
+VALUES
+	(DEFAULT, 'Robson', '1984-08-20', 'M', '84.0', '1.79', 'Brasil');
 
-select * from nome_da_tabela;
+SELECT * FROM nome_da_tabela;
 
 -- inserir usuários quando todas as informções forem padrão
-insert into nome_da_tabela values
-	(default, 'Ana', '1982-10-24', 'F', '56.0', '1.62', 'Brasil');
+INSERT INTO nome_da_tabela VALUES
+	(DEFAULT, 'Ana', '1982-10-24', 'F', '56.0', '1.62', 'Brasil');
 
 -- inserir varios usuários no banco
-insert into nome_da_tabela values
-	(default, 'Sofia Souza', '2016-03-21', 'F', '25.0', '1.20', 'Brasil'),
-    (default, 'Iris Souza', '1961-08-17', 'F', '60.0', '1.57', 'Brasil');
+INSERT INTO nome_da_tabela VALUES
+	(DEFAULT, 'Sofia Souza', '2016-03-21', 'F', '25.0', '1.20', 'Brasil'),
+    (DEFAULT, 'Iris Souza', '1961-08-17', 'F', '60.0', '1.57', 'Brasil');
 
 -- acrescentando coluna na tabela 
-alter table nome_da_tabela
-add column nome_da_coluna varchar (10);
+ALTER TABLE nome_da_tabela ADD column nome_da_coluna VARCHAR (10);
 
 -- acrescentando coluna na tabela escolhendo a posição 
-alter table nome_da_tabela
-add column nome_da_coluna varchar (10) after nome;
+ALTER TABLE nome_da_tabela ADD column nome_da_coluna VARCHAR (10) AFTER nome;
 
 -- acrescentando coluna na tabela escolendo a primeira posião da tabela
-alter table nome_da_tabela
-add column nome_da_coluna int first;
+ALTER TABLE nome_da_tabela ADD column nome_da_coluna INT FIRST;
 
 -- excluir coluna da tabela
-alter table nome_da_tabela
-drop column nome_da_coluna;
+ALTER TABLE nome_da_tabela DROP column nome_da_coluna;
 
 -- modificando as informações da coluna/campo da tabela
-alter table nome_da_tabela
-modify column nome_da_coluna varchar(20) default '';
+ALTER TABLE nome_da_tabela MODIFY column nome_da_coluna VARCHAR(20) DEFAULT '';
 
 -- modificando no nome da coluna
-alter table nome_da_tabela
-change column nome_da_coluna nome_para_modificar varchar(15) default 'Brasil' not null;
+ALTER TABLE nome_da_tabela change column nome_da_coluna nome_para_modificar VARCHAR(15) DEFAULT 'Brasil' not null;
 
 -- modificar o nome da tabela
-alter table nome_da_tabela
-rename to nome_para_modificar;
+ALTER TABLE nome_da_tabela rename TO nome_para_modificar;
 
 -- modificar a posição para primeiro
-alter table nome_da_tabela -- alter table - comando para manipular colunas
-add column nome_da_coluna int first;
+ALTER TABLE nome_da_tabela ADD column nome_da_coluna INT FIRST;
 
--- add a chave primária
-alter table nome da tabela
-add primary key(nome da colula que será chave primária);
+-- ADD a chave primária
+ALTER TABLE nome da tabela ADD PRIMARY KEY(nome da colula que será chave primária);
 
 -- apagar a tabela
-drop table if exists nome da tabela;
+DROP TABLE IF EXISTS nome da tabela;
 
 -- mudar os registros - Update --> Comando para moficar a linha da tabela
-update nome da tabela
-set nome_da_coluna = 'robson'
-where nome_da_coluna = 1;
+UPDATE nome da tabela SET nome_da_coluna = 'robson' WHERE nome_da_coluna = 1;
 
--- mudar dois regitro ou mais - sempre na mesma lina
-update nome_da_tabela
-set nome_da_coluna = 'Mysql' , nome_da_coluna = 'YouTube'
-where nome_da_coluna = 1
+ -- mudar dois regitro ou mais - sempre na mesma lina
+UPDATE nome_da_tabela SET nome_da_coluna = 'Mysql' nome_da_coluna = 'YouTube' WHERE nome_da_coluna = 1
 limit = 1; -- limitar a alteração para apenas uma linha, tirando o risco de fazer mudança em outra linha
 
 --modificar mais de duas linhas
-update nome_da_tabela
-	set nome_da_coluna = 'robson'
-	where nome_da_tabela = 1
+UPDATE nome_da_tabela 	SET nome_da_coluna = 'robson' WHERE nome_da_tabela = 1
 
--- remover linhas da tabela
-delete from nome_da_tabela
-where idcurso = '8'
+ -- remover linhas da tabela
+DELETE FROM nome_da_tabela WHERE idcurso = '8'
 
 -- deletar mais de um linha
-delete from cursos
-where idcurso = 2022 -- Se na tabela tiver mais de um usuário tiver no campo o ano 2022	
+DELETE FROM cursos WHERE i curso = 2022 -- Se na tabela tiver mais de um usuário tiver no campo o ano 2022	
 
 -- todas linhas de tabela
-truncate table cursos	
+truncate TABLE cursos	
 
 						-- COMANDOS USANDO SELECT--
-						
--- orderna a table pela colula
-select * from cursos
-order by nome;
 
--- orderna a table pela coluna de baixo para cima
-select * from cursos
-order by nome desc;
+-- Ordenar tabela order by					
+SELECT * FROM tabela_de_produtos order by preco_de_lista; -- orderna a TABLE pela colula - order by
+SELECT * FROM tabela_de_produtos order by preco_de_lista desc; -- orderna a TABLE pela coluna de baixo para cima
+SELECT * FROM tabela_de_produtos order by sabor, preco_de_lista; -- ordenar primeiro pela coluna sabor e depois preço
 
 -- selecionar coluna
-select idcurso, nome, ano from cursos -- posso colocar os campos invertidos
-order by nome, ano; -- qual coluna devo ordenar a consulta
+SELECT idcurso, nome, ano FROM cursos -- posso colocar os campos invertidos
 
 -- pesquisar o banco por linhas
-select * from nome_da_tabela
-where nome_da_tabela = '1'
-order by nome_da_tabela;
-
-
-
-
-select ano, idcurso from cursos
-where idcurso = '1'
-order by nome;
-
-select ano, idcurso,curso from cursos
-where idcurso > '1'
-order by nome;
+SELECT * FROM nome_da_tabela WHERE nome_da_coluna = '1'
+SELECT nome_da_coluna, nome_da_coluna FROM nome_da_tabelas WHERE nome_da_coluna = '1'
+SELECT nome_da_coluna, nome_da_coluna,nome_da_coluna FROM nome_da_tabela WHERE nome_da_coluna > '1'
 
 -- operadores relacionais - =, <,>,<=,>=,!=,<> 
+SELECT idcurso FROM cursos WHERE nome between 7 and 10;
+SELECT nome, totalaula FROM cursos WHERE totalaula in (35,36);
+SELECT ano, totalaula, nome FROM cursosWHERE ano < 2019 and totalaula > 40;
 
-select idcurso from cursos
-where nome between 7 and 10;
+SELECT ano, totalaula, nome FROM cursos
+WHERE ano < 2019 or totalaula > 40;
 
-select nome, totalaula from cursos
-where totalaula in (35,36);
-
-select ano, totalaula, nome from cursos
-where ano < 2019 and totalaula > 40;
-
-select ano, totalaula, nome from cursos
-where ano < 2019 or totalaula > 40;
-
--- Pesquisa com Like / Operador 'like = parecido' / % = caractere coringa
-select * from nome_da_tabela where nome_da_coluna like'%nome_para_pesquisar%';
+-- Pesquisa com Like / Operador 'LIKE = parecido' / % = caractere coringa
+SELECT * FROM nome_da_tabela WHERE nome_da_coluna LIKE'%nome_para_pesquisar%';
 
 -- pesquisa o p no inicio da palavr a
-select * from nome_da_tabela
+SELECT * FROM nome_da_tabela
 
-where nome_da_coluna like 'p%'; -- 
+WHERE nome_da_coluna LIKE 'p%'; -- 
 
 -- pesquisa o a no final da palavra
-select * from nome_da_tabela
-where nome like '%a';
+SELECT * FROM nome_da_tabela WHERE nome LIKE '%a';
 
 -- pesquisa o a em qualquer lugar
-select * from nome_da_tabela
-where nome like '%a%';
+SELECT * FROM nome_da_tabela WHERE nome LIKE '%a%';
 
 -- pesquisa tudo que não tiver a
-select * from nome_da_tabela
-where nome not like '%a%';
+SELECT * FROM nome_da_tabela WHERE nome not LIKE '%a%';
 
 -- pesquisa tudo que começa com p e termine n
-select * from nome_da_tabela
-where nome like 'p%n';
+SELECT * FROM nome_da_tabela WHERE nome LIKE 'p%n';
 
 -- pesquisa tudo que começa com p e termine n e + caractere
-select * from nome_da_tabela
-where nome like 'p%n_'; -- '_' sublinhado wild card
+SELECT * FROM nome_da_tabela WHERE nome LIKE 'p%n_'; -- '_' sublinhado wild card
 
--- Trocar o nome da coluna da tabela
+-- Trocar o nome da coluna da tabela quando pesquisamos - AS
 SELECT nome_da_coluna AS apelido, matricula AS ID FROM nome_da_tabela WHERE nome_da_coluna = '00235' AND nome_da_coluna = 'tijuca';
 SELECT nome_da_coluna AS apelido, matricula AS ID FROM nome_da_tabela WHERE NOT (nome_da_coluna = '00235' AND nome_da_coluna = 'tijuca');
+
+-- Operador in - Irá trazer todas informação que coNtem na pesquisa
+SELECT * FROM nome_da_tabela WHERE nome_da_coluna IN ('informação_da_coluna', 'informação_da_coluna');
 
 -- Operadores Lógicos And e OR 
-SELECT nome_da_coluna AS apelido, matricula AS ID FROM nome_da_tabela WHERE nome_da_coluna = '00235' AND nome_da_coluna = 'tijuca';
-SELECT nome_da_coluna AS apelido, matricula AS ID FROM nome_da_tabela WHERE NOT (nome_da_coluna = '00235' AND nome_da_coluna = 'tijuca');
+SELECT * FROM nome_da_tabela WHERE nome_da_coluna = '00235' AND nome_da_coluna = 'tijuca';
+SELECT * FROM nome_da_tabela WHERE NOT (nome_da_coluna = '00235' or nome_da_coluna = 'tijuca');
 
 -- Distinct - traz apenas o resumo 
-select distinct nome_da_coluna from nome_da_tabela;
-select distinct nome_da_coluna, nome_da_coluna from nome_da_tabela where nome_da_coluna = 'informação da tabela';
+SELECT distinct nome_da_coluna FROM nome_da_tabela;
+SELECT distinct nome_da_coluna, nome_da_coluna FROM nome_da_tabela WHERE nome_da_coluna = 'informação da tabela';
 
 -- Limitar a quantidade de linhas da pesquisa - Limit
-select * from nome_da_tabela limit 5; -- limita até 5 linhas na pesquisa
-select * from nome_da_tabela limit 2,15; -- começa da linha 2 da pesquisa e vai até a linha 15
+SELECT * FROM nome_da_tabela limit 5; -- limita até 5 linhas na pesquisa
+SELECT * FROM nome_da_tabela limit 2,15; -- começa da linha 2 da pesquisa e vai até a linha 15
 
 -- funçao de agregação
-select * from cursos order by totalaula;
-select count(*) from cursos;
-select count(*) from cursos where totalaula > 30;
+SELECT * FROM cursos order by totalaula; SELECT count(*) FROM cursos; SELECT count(*) FROM cursos WHERE totalaula > 30;
 
 -- maior
-select max(carga) from cursos;
+SELECT max(carga) FROM cursos;
 
 -- min
-select min(carga) from cursos;
+SELECT min(carga) FROM cursos;
 
 -- soma
-select sum(carga) from cursos;
+SELECT sum(carga) FROM cursos;
 
 -- média
-select avg(carga) from cursos;
-
-
-select nacionalidade, count(*) from familia -- contar a quantidade de vezes que aparece a informação na tabela
-group by nacionalidade
-order by nacionalidade;
-select * from familia where nacionalidade = 'brasil';
-
-
-select peso, count(*) from familia
-group by peso
-having peso >= '62.00';
-
---mudar o nome da coluna da tabela no select
-select * from tbcliente;
-select nome as nome_pri from tbcliente;
+SELECT avg(carga) FROM cursos;
 
 -- pesquisar data de nascimento
-select * from (Nome da tabela) where year(nome da coluna) = 1984
+SELECT * FROM (Nome da tabela) WHERE year(nome da coluna) = 1984
 
 
 
--- Operador in - Irá trazer todas informação que cotem na pesquisa
-select * from nome_da_tabela where nome_da_coluna in ('informação_da_coluna', 'informação_da_coluna');
